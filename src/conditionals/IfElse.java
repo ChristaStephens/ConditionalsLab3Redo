3ab157607d05d37f957a43d2f958d655f85c8317
Christa Stephens <ChristaStephens@outlook.com>
2019-03-06 21:40:27 -0500

    started if else

--- src/conditionals/IfElse.java
+++ src/conditionals/IfElse.java
@@ -1,5 +1,7 @@
 package conditionals;
 
+import java.util.Scanner;
+
 public class IfElse {
 
 	public static void main(String[] args) {
@@ -10,7 +12,14 @@ public class IfElse {
 		//if num is even and < 60 print num and  and even
 		//if num is odd and <60 print "odd and over 60"
 
+		int num;
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("Please enter a number between 1 and 100.");
 		
+		num = scnr.nextInt();
+		if (num %2==0 && num<25) {
+			
+		}
 	}
 
 }
