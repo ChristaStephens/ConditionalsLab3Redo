e38def445e4e783c30c652c6343a6f89206c6dae
Christa Stephens <ChristaStephens@outlook.com>
2019-03-06 21:33:06 -0500

    notes to begin

---
+++ src/conditionals/IfElse.java
@@ -0,0 +1,16 @@
+package conditionals;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		// Project Goals: user enters num between 1-100
+		//if num odd print odd
+		//if num even between 2-25 print "even & less than 25"
+		//if num entered is even between 26-60 print even
+		//if num is even and < 60 print num and  and even
+		//if num is odd and <60 print "odd and over 60"
+
+		
+	}
+
+}
